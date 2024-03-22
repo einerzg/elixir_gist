9033b7d348b5f526c83d3311a7f2b63a69ae8a05
einerzg <einerzg@gmail.com>
2024-03-22 09:12:56 -0500

    Add view form to create a gist

--- lib/elixir_gist_web/live/create_gist_live.ex
+++ lib/elixir_gist_web/live/create_gist_live.ex
@@ -1,7 +1,9 @@
 defmodule ElixirGistWeb.CreateGistLive do
   use ElixirGistWeb, :live_view
+  alias ElixirGist.{Gists, Gists.Gist}
 
   def mount(_params, _session, socket) do
+    socket = assign(socket, form: to_form(Gists.change_gist(%Gist{})))
     {:ok, socket}
   end
 end
